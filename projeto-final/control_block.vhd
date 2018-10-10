e41a9bab2e5b782f05f5eb56eb615567f31a545c
Lucas <zacchilucasm@gmail.com>
2018-10-10 10:04:16 -0300

    Functional Control Block

--- projeto-final/control_block.vhd
+++ projeto-final/control_block.vhd
@@ -6,86 +6,34 @@ entity control_block is
     generic (N: positive := 18);
     port (
         --control input
-        clock : in std_logic;
-        reset : in std_logic;
-        start : in std_logic;
-        enter : in std_logic;
-        comp_s: in std_logic;
+        clock: in std_logic;
+        reset: in std_logic;
+        start: in std_logic;
+        enter: in std_logic;
+        cmp_s: in std_logic;
         
         input: in std_logic_vector(N-1 downto 0);
              
         --control output
-        load_x : out std_logic;  -- enable register X
-        load_y : out std_logic;  -- enable register y
-        load_pi: out std_logic
-        
+        load_circle: out std_logic;
+        load_od: out std_logic;
+        load_pi: out std_logic;
+        load_total: out std_logic;
+        load_x : out std_logic;
+        load_y : out std_logic
     );
 end entity;
 
 architecture fsm of control_block is
-    type internal_state is (first_state, S0, S1, S2, S3, S4, S5);
+    type internal_state is (first_state, S0, S1, S2, S3, S4, S5, S6);
     signal next_state, current_state: internal_state;
+    
+    signal i: integer:= 0;
+    signal it: integer;
   
     
 begin
-
-    --Next State Logic
-    nsl: process (current_state, start)
-    begin
-        next_state <= current_state;
- 
-        case current_state is
-            when first_state=>
-                if (start = '1') then
-                    next_state <= S0;
-                else
-                    next_state <= first_state;
-                end if;
-              
-            when S0 =>
-                if (enter  = '1') then
-                    next_state <= S1;
-                else
-                    next_state <= S0;
-                end if;
-            
-            when S1 =>
-                for i in 0 to iterations loop
-                    if (i < iterations) then
-                        next_state <= S2;
-                    else
-                        next_state <= S5;
-                    end if;
-                end loop;
-            
-            when S2 =>
-                --initializes x and y with random numbers
-                --squares x and y
-                --adds x² and y²
-                load_x <= '1';
-                load_y <= '1';
-                
-                if comp_s <= '1' then
-                    next_state <= S3;
-                else
-                    next_state <= S4;
-                end if;
-                
-            when S3 =>
-                --in_circle ++
-                next_state <= S4;
-            when S4 =>
-                --total ++
-                next_state <= S1;
-            when S5 =>
-                --show result;
-                if (reset = '1') then
-                    next_state <= first_state;
-                else
-                    next_state <= S5;
-                end if;
-        end case;
-    end process;
+    it <= to_integer(unsigned(input));
     
     --Memory Element
     memory: process (clock, reset)
@@ -96,5 +44,75 @@ begin
             current_state <= next_state;
         end if;
      end process;
+    
+    
+    --Next State Logic
+    nsl: process (current_state, start, enter, input)
+    begin
+        next_state <= current_state;
+        
+        case current_state is
+            when first_state=>
+                if (start = '1') then
+                    next_state <= S0;
+                else
+                    next_state <= first_state;
+                end if;
+              
+            when S0 =>
+            
+                if (enter  = '1') then
+                    next_state <= S1;
+                else
+                    next_state <= S0;
+                end if;
+            
+            when S1 =>
+                
+                if (i < it) then
+                    next_state <= S2;
+                else
+                    next_state <= S6;
+                end if;
+                
+                i<= i + 1;
+                
+                
+            when S2 =>
+                next_state <= S3;
+                
+            when S3 =>
+
+                if (cmp_s = '1') then
+                    next_state <= S4;
+                else
+                    next_state <= S5;
+                end if;
+            
+            when S4 =>
+                next_state <= S5;
+            
+            when S5 =>
+                next_state <= S1;
+            
+            when S6 =>
+                if (reset = '1') then
+                    next_state <= first_state;
+                else
+                    next_state <= S6;
+                end if;
+                
+        end case;
+    end process;
+    
+    --output logic
+    load_x <= '1' when current_state = S2 else '0';
+    load_y <= '1' when current_state = S2 else '0';
+    
+    load_od <= '1' when current_state = S3 else '0';
+    
+    load_circle <= '1' when current_state = S4 else '0';
+    load_total <= '1' when current_state = S5 else '0';
+    load_pi <= '1' when current_state = S6 else '0';
     
 end architecture;
